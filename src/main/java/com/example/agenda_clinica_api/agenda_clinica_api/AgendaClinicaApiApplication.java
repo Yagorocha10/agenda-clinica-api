c59423c9d478374289d038c7974010b4d0cda495
Yagorocha10 <yagodasilvarocha30@gmail.com>
2026-04-15 13:50:43 -0300

    feat: estrutura inicial do projeto

---
+++ src/main/java/com/example/agenda_clinica_api/agenda_clinica_api/AgendaClinicaApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.agenda_clinica_api.agenda_clinica_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaClinicaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaClinicaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/agenda_clinica_api/agenda_clinica_api/AgendaClinicaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.agenda_clinica_api.agenda_clinica_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaClinicaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
